5f8773c10be22dba1665ca60bf987726517bab35
HallerPatrick <HallerPatrick@users.noreply.github.com>
2021-09-07 15:39:35 +0000

    deploy: 0f1bfa66d1938a184c71298897848b2f3e6a5f07

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["liva_parser"] = {"name":"","dirs":[{"name":"parser","dirs":[{"name":"expression","files":["binary.rs","call.rs","mod.rs"]},{"name":"statement","dirs":[{"name":"declaration","files":["assignment.rs","class.rs","for_statement.rs","function.rs","if_statement.rs","mod.rs","while_statement.rs"]}],"files":["import.rs","mod.rs"]}],"files":["literals.rs","mod.rs","tokens.rs"]}],"files":["lib.rs"]};
+sourcesIndex["liva_parser"] = {"name":"","dirs":[{"name":"parser","dirs":[{"name":"expression","files":["binary.rs","call.rs","mod.rs"]},{"name":"statement","dirs":[{"name":"declaration","files":["assignment.rs","class.rs","for_statement.rs","function.rs","if_statement.rs","mod.rs","while_statement.rs"]}],"files":["import.rs","mod.rs"]}],"files":["comment.rs","literals.rs","mod.rs","tokens.rs"]}],"files":["lib.rs"]};
 createSourceSidebar();
