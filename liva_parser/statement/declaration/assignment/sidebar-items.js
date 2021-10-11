9dbed1f79c51576b00ff393dbb643ea237bf6e1f
HallerPatrick <HallerPatrick@users.noreply.github.com>
2021-10-11 22:39:41 +0000

    deploy: 5fa2d15e80a14cf45253fd0173ea22cb8b2271da

--- liva_parser/statement/declaration/assignment/sidebar-items.js
+++ liva_parser/statement/declaration/assignment/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Assignment","A assignment is a statement, while a re-assignemnt is an epression so we only parse a statement here, therefore is the let mandatory"],["LAssignment","A assignment is a statement, while a re-assignemnt is an epression so we only parse a statement here, therefore is the let mandatory"]]});
+initSidebarItems({"struct":[["Assignment",""],["LAssignment","A assignment is a statement, while a re-assignemnt is an epression so we only parse a statement here, therefore is the let mandatory"]]});
